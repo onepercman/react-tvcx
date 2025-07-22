5eb123b33f646bee1075f50cf3f1ad913291548d
onepercman <onepercman@gmail.com>
2025-07-22 11:27:08 +0700

    🚀 3.0.5

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.tsx'],
-  format: ['cjs', 'esm'],
-  external: ['tailwindcss'],
+  format: ['esm', 'cjs'],
   dts: true,
-  sourcemap: true,
+  clean: true,
 })
